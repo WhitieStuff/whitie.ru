1c0947afd0ed8bbe36fb5d618ff4213cac5794d7
Alexander White <48159863+WhitieStuff@users.noreply.github.com>
2022-04-08 06:25:33 +0300

    0.2.0

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let enter = document.querySelector('#enter')
+let landing = document.querySelector('#landing')
+
+enter.addEventListener('click', () => {
+  landing.classList.add('landing_hidden')
+})
